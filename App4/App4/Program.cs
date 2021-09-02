62f46ba859781586fccff63b720b39b3f017cf22
Kostya Osauleno <kanstantin32@gmail.com>
2021-09-02 11:31:40 +0300

    add StyleCop

--- App4/App4/Program.cs
+++ App4/App4/Program.cs
@@ -2,8 +2,15 @@
 
 namespace App4
 {
+    /// <summary>
+    /// Start point of general app.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Start  of general app.
+        /// </summary>
+        /// <param name="args">args added via console.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
