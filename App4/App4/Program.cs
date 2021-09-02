311377173ef6de8aa3ef80104a0adebd1b422d05
Kostya Osauleno <kanstantin32@gmail.com>
2021-09-02 13:41:27 +0300

    filling the array with random values from 1 to 26 include

--- App4/App4/Program.cs
+++ App4/App4/Program.cs
@@ -13,7 +13,26 @@ namespace App4
         /// <param name="args">args added via console.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter size of array");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] randomarray = new int[n];
+            randomarray = GetRandomeArray(n, 1, 26);
+            foreach (int i in randomarray)
+            {
+                Console.Write($"{i} ");
+            }
+
+            static int[] GetRandomeArray(int size, int min, int max)
+            {
+                var r = new Random();
+                int[] arr = new int[size];
+                for (int i = 0; i < size; i++)
+                {
+                    arr[i] = r.Next(min, max + 1);
+                }
+
+                return arr;
+            }
         }
     }
 }
